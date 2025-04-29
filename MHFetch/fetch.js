dd7ace7d1eb097777518019b87dbef6ed404dd55
solvek <adamchuk@gmail.com>
2025-04-29 18:59:19 +0300

    GM_xmlhttpRequest request

--- MHFetch/fetch.js
+++ MHFetch/fetch.js
@@ -6,8 +6,7 @@
 // @author       You
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        GM_getValue
-// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
@@ -102,28 +101,26 @@
         btn.style.fontSize = '14px';
 
         btn.addEventListener('click', function() {
-            fetch("https://familygraphql.myheritage.com/fetch_dna_matches_for_kit/", {
-  "headers": {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7,ru;q=0.6,pl;q=0.5",
-    "cache-control": "no-cache",
-    "content-type": "multipart/form-data; boundary=----WebKitFormBoundary8iXPlzd7pa1joZDC",
-    "pragma": "no-cache",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Linux\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site"
-  },
-  "referrer": "https://www.myheritage.com.ua/",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"bearer_token\"\r\n\r\n4.cbdd04f731661d487d728136bd8331a0.71244161.1745938963.1440.7345153..6g0ibucs.3b6fd8806cf689b511d71445fe2493f6b15bb0577dd16a09636395c5570f9078\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"query\"\r\n\r\n\"{dna_kit(id:\\\"dnakit-FE4A3EF6-BE08-4258-B60A-3DBB86BAB039\\\",lang:\\\"UK\\\"){dna_matches(offset:270,limit:10,sort_query:\\\"total_shared_segments_length_in_cm\\\",query:\\\"\\\",filter:\\\"0\\\",filter_by_relationship:\\\"0\\\",filter_by_country:\\\"0\\\",filter_by_labels:\\\"\\\"){data{id link is_new complete_dna_relationships{relationship_type relationship_degree}refined_dna_relationships{relationship_type relationship_degree}dna_cm_explainer{relationships{...dna_match_relationship}most_probable_relationships{...dna_match_relationship}calculation_strategy}exact_dna_relationship genealogical_relationship total_shared_segments_length_in_cm largest_shared_segment_length_in_cm percentage_of_shared_segments total_shared_segments is_recently_recalculated confidence_level other_dna_kit{submitter{id name name_transliterated first_name first_name_transliterated link is_public}member{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}country_code country is_public link}associated_individual{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}birth_place tree{...tree_info}relationship{...RELATIONSHIP_INFO}link_in_pedigree_tree link_in_tree}}}}}}fragment dna_match_relationship on DnaMatchRelationship{relationship_type relationship_class path_type probability most_recent_common_ancestor_relationship_type most_recent_common_ancestor_relationship_class}fragment tree_info on Tree{id name link individual_count site{is_request_membership_allowed creator{id name name_transliterated country country_code link is_public}}}fragment PERSONAL_PHOTO_INFO on Photo{thumbnails(thumbnail_size:\\\"96x96\\\"){url}}fragment RELATIONSHIP_INFO on Relationship{relationship_description}\"\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\n\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"variables\"\r\n\r\n\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"site_id\"\r\n\r\n80443353\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\nFETCH_DNA_MATCHES_FOR_KIT\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"mhc#PHPSESSID\"\r\n\r\n2df5d3f8009a34e15466908befa6d148\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC--\r\n",
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "omit"
-});
+            const bodyData = `------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"bearer_token\"\r\n\r\n4.cbdd04f731661d487d728136bd8331a0.71244161.1745938963.1440.7345153..6g0ibucs.3b6fd8806cf689b511d71445fe2493f6b15bb0577dd16a09636395c5570f9078\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"query\"\r\n\r\n\"{dna_kit(id:\\\"dnakit-FE4A3EF6-BE08-4258-B60A-3DBB86BAB039\\\",lang:\\\"UK\\\"){dna_matches(offset:10,limit:10,sort_query:\\\"total_shared_segments_length_in_cm\\\",query:\\\"\\\",filter:\\\"0\\\",filter_by_relationship:\\\"0\\\",filter_by_country:\\\"0\\\",filter_by_labels:\\\"\\\"){data{id link is_new complete_dna_relationships{relationship_type relationship_degree}refined_dna_relationships{relationship_type relationship_degree}dna_cm_explainer{relationships{...dna_match_relationship}most_probable_relationships{...dna_match_relationship}calculation_strategy}exact_dna_relationship genealogical_relationship total_shared_segments_length_in_cm largest_shared_segment_length_in_cm percentage_of_shared_segments total_shared_segments is_recently_recalculated confidence_level other_dna_kit{submitter{id name name_transliterated first_name first_name_transliterated link is_public}member{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}country_code country is_public link}associated_individual{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}birth_place tree{...tree_info}relationship{...RELATIONSHIP_INFO}link_in_pedigree_tree link_in_tree}}}}}}fragment dna_match_relationship on DnaMatchRelationship{relationship_type relationship_class path_type probability most_recent_common_ancestor_relationship_type most_recent_common_ancestor_relationship_class}fragment tree_info on Tree{id name link individual_count site{is_request_membership_allowed creator{id name name_transliterated country country_code link is_public}}}fragment PERSONAL_PHOTO_INFO on Photo{thumbnails(thumbnail_size:\\\"96x96\\\"){url}}fragment RELATIONSHIP_INFO on Relationship{relationship_description}\"\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\n\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"variables\"\r\n\r\n\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"site_id\"\r\n\r\n80443353\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\nFETCH_DNA_MATCHES_FOR_KIT\r\n------WebKitFormBoundary8iXPlzd7pa1joZDC--\r\n`;
+
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: "https://familygraphql.myheritage.com/fetch_dna_matches_for_kit/",
+                headers: {
+                    "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundary8iXPlzd7pa1joZDC",
+                    "accept": "application/json, text/plain, */*",
+                    "cache-control": "no-cache",
+                    "pragma": "no-cache"
+                },
+                data: bodyData,
+                onload: function (response) {
+                    console.log("✅ Response received:", response.responseText);
+                },
+                onerror: function (err) {
+                    console.error("❌ Request failed:", err);
+                }
+            });
+
             /*
 
             if (mhFetchState.currentPage != null){
