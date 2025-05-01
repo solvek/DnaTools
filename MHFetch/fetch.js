4a0d93904e526f049c1c180ec73b480492a6cdd7
solvek <adamchuk@gmail.com>
2025-05-01 18:52:17 +0300

    Fetching matches fine

--- MHFetch/fetch.js
+++ MHFetch/fetch.js
@@ -14,54 +14,97 @@
 
     const PAGE_SIZE = 100;
 
-    const mhFetchState = loadData();
+    var mhFetchState = loadData();
 
     var auth = null;
+    var kitId = null;
+    var started = false;
 
     const originalOpen = XMLHttpRequest.prototype.open;
     const originalSend = XMLHttpRequest.prototype.send;
-    //const originalSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
 
     XMLHttpRequest.prototype.open = function(method, url) {
         this._url = url;
         return originalOpen.apply(this, arguments);
     };
 
-/*
-    XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
-        if (name == "Authorization"){
-            auth = value.split(" ")[1];
-            console.log("✅ New auth:", auth);
-        }
-        return originalSetRequestHeader.apply(this, arguments);
-    }
-    */
-
-
     XMLHttpRequest.prototype.send = function(body) {
+         if (this._url.includes("familygraphql.myheritage.com/fetch_dna_kit_ethnicity_estimate_overview/")) {
+             this.addEventListener('load', function(){
+                 try {
+                     kitId = JSON.parse(this.responseText).data.dna_kit.id.substring(7)
+                     console.log('Current kit id: ', kitId);
+                     startApp();
+                 }
+                 catch(e){
+                     console.error("Failted to determine kitId", e);
+                 }
+             });
+        }
+
         if (this._url.includes("familygraphql.myheritage.com/") && (body instanceof FormData)) {
             const token = body.get("bearer_token");
             if (token !== null) {
                 console.log("🔐 Bearer token:", token);
                 auth = token;
+                startApp();
             }
         }
 
-        /*
-        if (this._url.includes("familygraphql.myheritage.com/fetch_dna_kit_ethnicity_estimate_overview/")) {
-            mhFetchState.kitId = JSON.parse(this.responseText).data.dna_kit.id.substring(7)
-            console.log('Current kit id: ', mhFetchState.kitId);
+        return originalSend.apply(this, arguments);
+    };
+
+    function startApp(){
+        if (started) return;
+
+        if (!auth) return;
+
+        if (mhFetchState == null){
+            if (kitId == null) return;
+            showButton();
+        }
+        else {
+            startFetching();
         }
 
-        if (this._url.includes("familygraphql.myheritage.com/fetch_dna_matches_features_for_kit/")) {
-            mhFetchState.matchesCount = JSON.parse(this.responseText).data.dna_kit.dna_matches.count;
+        started = true;
+    }
+
+    function startFetching(){
+        if (mhFetchState.matchesCount == null){
+            fetchMatchesCount();
+            return;
         }
 
-        if (this._url.includes("familygraphql.myheritage.com/fetch_dna_matches_for_kit/")) {
-            if (mhFetchState.currentPage != null){
+        fetchMatchesPage();
+    }
+
+    function fetchMatchesCount(){
+        performRequest(
+            "fetch_dna_matches_features_for_kit",
+            '------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"bearer_token\"\r\n\r\n'+auth+'\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"query\"\r\n\r\n\"{dna_kit(id:\\\"dnakit-'+mhFetchState.kitId+'\\\",lang:\\\"UK\\\"){dna_labels{data{id name color_code}}dna_matches(offset:490,limit:10,sort_query:\\\"total_shared_segments_length_in_cm\\\",query:\\\"\\\",filter:\\\"0\\\",filter_by_relationship:\\\"0\\\",filter_by_country:\\\"0\\\",filter_by_labels:\\\"\\\"){count data{id is_favorite match_notes{data{id text}}match_labels{data{id}}ancestral_places{data{number_of_other_ancestors number_of_ancestors country_code state_code is_obfuscated}}theories_summary{id is_new number_of_theories best_theory_relationship{description}confirmation_status}common_ancestors{data{surname name{first_name last_name}other_name{first_name last_name}name_count other_name_count closest_relationship other_closest_relationship closest_relationship_side other_closest_relationship_side}}}}}}\"\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"variables\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"site_id\"\r\n\r\n80443353\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\nFETCH_DNA_MATCHES_FEATURES_FOR_KIT\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"mhc#PHPSESSID\"\r\n\r\ne302870e8aba4902fee469cdb250d4b4\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3--\r\n',
+            function(json){
+                mhFetchState.matchesCount == json.data.dna_kit.dna_matches.count;
+                storeData(mhFetchState);
+                fetchMatchesPage();
+            }
+        );
+    }
+
+    function fetchMatchesPage(offset){
+        if (offset){
+            storeData(mhFetchState);
+        }
+        else {
+            offset = mhFetchState.matchesOffset || 0;
+        }
+        performRequest(
+            "fetch_dna_matches_for_kit",
+            '------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"bearer_token\"\r\n\r\n'+auth+'\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"query\"\r\n\r\n\"{dna_kit(id:\\\"dnakit-'+mhFetchState.kitId+'\\\",lang:\\\"UK\\\"){dna_matches(offset:'+offset+',limit:'+PAGE_SIZE+',sort_query:\\\"total_shared_segments_length_in_cm\\\",query:\\\"\\\",filter:\\\"0\\\",filter_by_relationship:\\\"0\\\",filter_by_country:\\\"0\\\",filter_by_labels:\\\"\\\"){data{id link is_new complete_dna_relationships{relationship_type relationship_degree}refined_dna_relationships{relationship_type relationship_degree}dna_cm_explainer{relationships{...dna_match_relationship}most_probable_relationships{...dna_match_relationship}calculation_strategy}exact_dna_relationship genealogical_relationship total_shared_segments_length_in_cm largest_shared_segment_length_in_cm percentage_of_shared_segments total_shared_segments is_recently_recalculated confidence_level other_dna_kit{submitter{id name name_transliterated first_name first_name_transliterated link is_public}member{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}country_code country is_public link}associated_individual{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}birth_place tree{...tree_info}relationship{...RELATIONSHIP_INFO}link_in_pedigree_tree link_in_tree}}}}}}fragment dna_match_relationship on DnaMatchRelationship{relationship_type relationship_class path_type probability most_recent_common_ancestor_relationship_type most_recent_common_ancestor_relationship_class}fragment tree_info on Tree{id name link individual_count site{is_request_membership_allowed creator{id name name_transliterated country country_code link is_public}}}fragment PERSONAL_PHOTO_INFO on Photo{thumbnails(thumbnail_size:\\\"96x96\\\"){url}}fragment RELATIONSHIP_INFO on Relationship{relationship_description}\"\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"variables\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\nFETCH_DNA_MATCHES_FOR_KIT\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3--\r\n',
+            function(json){
                 mhFetchState.matches = [];
 
-                const raw = JSON.parse(this.responseText).data.dna_kit.dna_matches.data;
+                const raw = json.data.dna_kit.dna_matches.data;
 
                 for (let m of raw) {
                     try {
@@ -84,34 +127,49 @@
                     }
                 }
 
-                download(JSON.stringify(mhFetchState), "application/json", "matches-"+String(mhFetchState.currentPage).padStart(5, '0')+".json");
+                download(JSON.stringify(mhFetchState), "application/json", "matches-"+String(offset).padStart(6, '0')+".json");
 
-                const nextPage = mhFetchState.currentPage + 1;
-                mhFetchState.matches = null;
-                if ((nextPage > totalPages(PAGE_SIZE, mhFetchState.matchesCount)) || (nextPage >=5)){
-                    mhFetchState.currentPage = null;
-                    storeData(mhFetchState);
-                    console.log("Fetch finished");
+                offset += PAGE_SIZE;
+
+                if (offset < mhFetchState.matcheCount || offset < 800){
+                    mhFetchState.matches = null;
+                    fetchMatchesPage(offset);
                 }
                 else {
-                    fetchMatchesPage(nextPage);
+                    mhFetchState = null;
+                    clearData();
                 }
             }
-        }
+        );
+    }
 
-        */
-        return originalSend.apply(this, arguments);
-    };
-
-    window.addEventListener('load', function() {
-        showButton();
-    });
+    function performRequest(endpoint, body, onResult){
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://familygraphql.myheritage.com/"+endpoint+"/",
+            headers: {
+                "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryHRCxw6zSWoDXD7V3",
+                "accept": "application/json, text/plain, */*",
+                "cache-control": "no-cache",
+                "pragma": "no-cache"
+            },
+            data: body,
+            onload: function (response) {
+                //console.log("✅ Response received:", response.responseText);
+                const j = JSON.parse(response.responseText);
+                onResult(j);
+            },
+            onerror: function (err) {
+                console.error("❌ Request "+endpoint+" failed:", err);
+            }
+        });
+    }
 
     function showButton(){
         const btn = document.createElement('button');
         btn.innerText = "Fetch graph";
         btn.style.position = 'fixed';
-        btn.style.top = '20px';
+        btn.style.top = '60px';
         btn.style.right = '20px';
         btn.style.zIndex = 10000;
         btn.style.padding = '10px 15px';
@@ -123,54 +181,13 @@
         btn.style.fontSize = '14px';
 
         btn.addEventListener('click', function() {
-            const bodyData = '------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"bearer_token\"\r\n\r\n'+auth+'\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"query\"\r\n\r\n\"{dna_kit(id:\\\"dnakit-FE4A3EF6-BE08-4258-B60A-3DBB86BAB039\\\",lang:\\\"UK\\\"){dna_matches(offset:1220,limit:10,sort_query:\\\"total_shared_segments_length_in_cm\\\",query:\\\"\\\",filter:\\\"0\\\",filter_by_relationship:\\\"0\\\",filter_by_country:\\\"0\\\",filter_by_labels:\\\"\\\"){data{id link is_new complete_dna_relationships{relationship_type relationship_degree}refined_dna_relationships{relationship_type relationship_degree}dna_cm_explainer{relationships{...dna_match_relationship}most_probable_relationships{...dna_match_relationship}calculation_strategy}exact_dna_relationship genealogical_relationship total_shared_segments_length_in_cm largest_shared_segment_length_in_cm percentage_of_shared_segments total_shared_segments is_recently_recalculated confidence_level other_dna_kit{submitter{id name name_transliterated first_name first_name_transliterated link is_public}member{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}country_code country is_public link}associated_individual{id first_name first_name_transliterated name name_transliterated gender age_group age_group_in_years personal_photo{...PERSONAL_PHOTO_INFO}birth_place tree{...tree_info}relationship{...RELATIONSHIP_INFO}link_in_pedigree_tree link_in_tree}}}}}}fragment dna_match_relationship on DnaMatchRelationship{relationship_type relationship_class path_type probability most_recent_common_ancestor_relationship_type most_recent_common_ancestor_relationship_class}fragment tree_info on Tree{id name link individual_count site{is_request_membership_allowed creator{id name name_transliterated country country_code link is_public}}}fragment PERSONAL_PHOTO_INFO on Photo{thumbnails(thumbnail_size:\\\"96x96\\\"){url}}fragment RELATIONSHIP_INFO on Relationship{relationship_description}\"\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"variables\"\r\n\r\n\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\nFETCH_DNA_MATCHES_FOR_KIT\r\n------WebKitFormBoundaryHRCxw6zSWoDXD7V3--\r\n';
-
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: "https://familygraphql.myheritage.com/fetch_dna_matches_for_kit/",
-                headers: {
-                    "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryHRCxw6zSWoDXD7V3",
-                    "accept": "application/json, text/plain, */*",
-                    "cache-control": "no-cache",
-                    "pragma": "no-cache"
-                },
-                data: bodyData,
-                onload: function (response) {
-                    console.log("✅ Response received:", response.responseText);
-                },
-                onerror: function (err) {
-                    console.error("❌ Request failed:", err);
-                }
-            });
-
-            /*
-
-            if (mhFetchState.currentPage != null){
-                mhFetchState.currentPage = null;
-                storeData(mhFetchState);
-                return;
-            }
-
-            if (mhFetchState.kitId == null){
-                alert("Current kit id not defined. Enter DNA section");
-                return;
-            }
-
+            btn.remove();
+            mhFetchState = {kitId: kitId};
+            storeData(mhFetchState);
             startFetching();
-            */
         });
 
         document.body.appendChild(btn);
-    }
-
-    function startFetching(){
-        fetchMatchesPage(1);
-    }
-
-    function fetchMatchesPage(pageNumber){
-        mhFetchState.currentPage = pageNumber;
-        storeData(mhFetchState);
-        navigate("/dna/matches?kitId="+mhFetchState.kitId+"&ps="+PAGE_SIZE+"&p="+pageNumber)
     }
 
     function storeData(obj){
@@ -180,7 +197,8 @@
     }
 
     function loadData(){
-        return JSON.parse(localStorage.getItem('mhFetchState') || '{}');
+        const j = localStorage.getItem('mhFetchState');
+        return j ? JSON.parse(j) : null;
     }
 
     function clearData(){
